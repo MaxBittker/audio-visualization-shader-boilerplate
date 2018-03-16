7bdc7143c62e9bd3d3c41fc3fa8a480e5cb90439
maxbittker <maxbittker@gmail.com>
2018-03-15 22:52:14 -0700

    reduce garbage

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ let vsh = require("./vertex.glsl");
 
 const lastFrame = regl.texture();
 const pixels = regl.texture();
+let audioBuffer = null;
+
 let audioVisualization = (audio, {}) => {
   let drawTriangle = regl({
     frag: fsh,
     uniforms: {
       // webcam,
       bands: () => {
-        var dataArray = new Uint8Array(audio.frequencyBinCount);
-        audio.getByteFrequencyData(dataArray);
         let bands = new Array(4);
         var k = 0;
         var f = 0.0;
@@ -27,38 +27,35 @@ let audioVisualization = (audio, {}) => {
           c = 24,
           d = 512,
           i = 0;
-        for (; i < a; i++) f += dataArray[i];
+        for (; i < a; i++) f += audioBuffer[i];
         f *= 0.2; // 1/(a-0)
         f *= 0.003921569; // 1/255
         bands[0] = f;
         f = 0.0;
-        for (; i < b; i++) f += dataArray[i];
+        for (; i < b; i++) f += audioBuffer[i];
         f *= 0.166666667; // 1/(b-a)
         f *= 0.003921569; // 1/255
         bands[1] = f;
         f = 0.0;
-        for (; i < c; i++) f += dataArray[i];
+        for (; i < c; i++) f += audioBuffer[i];
         f *= 0.076923077; // 1/(c-b)
         f *= 0.003921569; // 1/255
         bands[2] = f;
         f = 0.0;
-        for (; i < d; i++) f += dataArray[i];
+        for (; i < d; i++) f += audioBuffer[i];
         f *= 0.00204918; // 1/(d-c)
         f *= 0.003921569; // 1/255
         bands[3] = f;
         return bands
       },
       spectrum: () => {
-        var dataArray = new Uint8Array(audio.frequencyBinCount); // Uint8Array should be the same length as the frequencyBinCount
-        audio.getByteFrequencyData(dataArray);
         return regl.texture({
-          width: dataArray.length,
+          width: audioBuffer.length,
           height: 1,
-          data: new Uint8Array(_.flatMap(dataArray, i => [i, i, i, i]))
+          data: new Uint8Array(_.flatMap(audioBuffer, i => [i, i, i, i]))
         });
       },
       // videoResolution: [videoWidth, videoHeight],
-      // Becomes `uniform float t`  and `uniform vec2 resolution` in the shader.
       t: ({ tick }) => tick / 100,
       resolution: ({ viewportWidth, viewportHeight }) => [
         viewportWidth,
@@ -69,9 +66,6 @@ let audioVisualization = (audio, {}) => {
       // See: https://github.com/regl-project/regl/blob/gh-pages/API.md#uniforms
     },
 
-    /*
-Attributes you don't need to modify if you just want to write full bleed fragment shaders:
-*/
     vert: vsh,
     attributes: {
       // Full screen triangle
@@ -83,7 +77,11 @@ Attributes you don't need to modify if you just want to write full bleed fragmen
 
   regl.frame(function(context) {
     window.a = audio;
-
+    if( !audioBuffer){
+      audioBuffer = new Uint8Array(audio.frequencyBinCount);
+    }
+    audio.getByteFrequencyData(audioBuffer);
+    
     regl.clear({
       color: [0, 0, 0, 1]
     });
@@ -99,7 +97,3 @@ let audio = audioAnalyzer({
   done: audioVisualization
 });
 
-// let cam = wc({
-//   regl,
-//   done: audio
-// });
